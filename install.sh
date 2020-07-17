1a3a97f15790c1a4c99a409c727147b15c37c0b3
julenvitoria <36037023+julenvitoria@users.noreply.github.com>
2020-07-17 19:13:24 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+sudo apt install libpng-dev -y
+sudo apt-get install python-gpiozero -y
+sudo apt-get install python-pkg-resources python3-pkg-resources -y
+sudo apt-get install build-essential python-dev python-smbus python-pip -y
+sudo chmod 755 /home/pi/temperatureMonitor/Pngview/pngview
+cd ~/temperatureMonitor
+crontab mycron
